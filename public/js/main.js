81bedda4e2910202e393745312899be1acdb372f
Egor Berezovskiy <ebe@zenexity.com>
2015-07-18 14:01:28 +0200

    Add libraries

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+var socket = window.io(document.location.host);
+
+var temperaturesData = [
+  { label: 'Temperature', values: [{time: new Date().getTime(), y: 0}] }
+];
+
+var areaChartInstance = $('#chart-temp').epoch({
+    type: 'time.area',
+    //pixelRatio: 1,
+    data: temperaturesData,
+    axes: ['top', 'right', 'bottom', 'left'],
+    fps: 60
+});
+
+socket.connect();
+socket.on('data', function (data) {
+  console.log("on", data);
+  if (data.data.type === 'temperature') {
+    console.log({
+      time: data.data.time,
+      y: data.data.value
+    });
+    temperaturesData[0].values.push({
+      time: data.data.time,
+      y: data.data.value
+    });
+    areaChartInstance.push([{
+      time: data.data.time,
+      y: data.data.value
+    }]);
+  }
+});
